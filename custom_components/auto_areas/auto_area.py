c91ed5ab4ab753b844648eb20ca5b09545d1eea4
Christian Stangier <christian.stangier@moia.io>
2021-12-20 21:55:19 +0100

    Fetch area through device  if not stored directly at entity

--- custom_components/auto_areas/auto_area.py
+++ custom_components/auto_areas/auto_area.py
@@ -7,6 +7,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.area_registry import AreaEntry
 from homeassistant.const import EVENT_HOMEASSISTANT_STARTED
 from homeassistant.helpers.entity_registry import EntityRegistry, RegistryEntry
+from homeassistant.helpers.device_registry import DeviceRegistry
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -34,13 +35,17 @@ class AutoArea(object):
         entity_registry: EntityRegistry = (
             await self.hass.helpers.entity_registry.async_get_registry()
         )
+        device_registry: DeviceRegistry = (
+            await self.hass.helpers.device_registry.async_get_registry()
+        )
 
         # Collect entities for this area
         for _entity_id, entity in entity_registry.entities.items():
+            # _LOGGER.debug("Evaluating entity %s", entity_id)
             if not is_valid(entity):
                 continue
 
-            if not get_area_id(entity) == self.area_id:
+            if not get_area_id(entity, device_registry) == self.area_id:
                 continue
 
             self.entities.append(entity)
@@ -59,11 +64,18 @@ def is_valid(entity: RegistryEntry) -> bool:
     return True
 
 
-def get_area_id(entity: RegistryEntry) -> Optional[str]:
+def get_area_id(
+    entity: RegistryEntry, device_registry: DeviceRegistry
+) -> Optional[str]:
     """Determines area_id from a registry entry"""
     # Check entity_id of entity:
     if entity.area_id is not None:
         return entity.area_id
-    # Check entity_id of device via device_registry
+
+    # Check area of device from device registry
+    if entity.device_id is not None:
+        device = device_registry.devices[entity.device_id]
+        if device is not None:
+            return device.area_id
 
     return None
