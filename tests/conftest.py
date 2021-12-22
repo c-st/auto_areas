cf2f3a093d6e8d037726ff207c3b22818c90e81d
Christian Stangier <christian.stangier@moia.io>
2021-12-22 01:07:14 +0100

    Extract test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,80 @@
 """Fixtures for testing."""
+from collections import OrderedDict
+import logging
+from homeassistant.util import slugify
 import pytest
+
+from pytest_homeassistant_custom_component.common import (
+    mock_area_registry,
+    mock_device_registry,
+    mock_registry,
+)
+from homeassistant.helpers import (
+    device_registry as dev_reg,
+    entity_registry as ent_reg,
+)
+
+_LOGGER = logging.getLogger(__name__)
+AREAS = ("Kitchen", "Living Room", "Bathroom", "Bedroom")
 
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
+    """Required for testing integration"""
     yield
+
+
+@pytest.fixture(name="areas", autouse=True)
+def fixture_areas(hass) -> dict:
+    """Create and provide Areas"""
+    area_registry = mock_area_registry(hass)
+    areas = OrderedDict()
+    for area in AREAS:
+        created_area = area_registry.async_create(area)
+        areas[slugify(area)] = created_area
+
+    return areas
+
+
+@pytest.fixture(name="device_registry")
+def fixture_device_registry(hass) -> dev_reg.DeviceRegistry:
+    """Provide a mock device registry"""
+    return mock_device_registry(hass)
+
+
+@pytest.fixture(name="entity_registry")
+def fixture_entity_registry(hass) -> ent_reg.EntityRegistry:
+    """Entities assigned to areas"""
+    default_entities = {
+        "light.entrance": ent_reg.RegistryEntry(
+            entity_id="light.entrance",
+            unique_id="entrance1",
+            platform="test",
+        ),
+        "light.entrance2": ent_reg.RegistryEntry(
+            entity_id="light.entrance2",
+            unique_id="entrance2",
+            platform="test",
+        ),
+    }
+    return mock_registry(
+        hass,
+        default_entities,
+    )
+
+
+def create_entity(
+    entity_registry: ent_reg.EntityRegistry,
+    domain: str,
+    unique_id: str,
+    area_id: str = None,
+    device_id: str = None,
+) -> ent_reg.RegistryEntry:
+    """Inserts a fake entity into registry"""
+    return entity_registry.async_get_or_create(
+        domain=domain,
+        unique_id=unique_id,
+        platform=area_id,
+        area_id=area_id,
+        device_id=device_id,
+    )
--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,113 +1,65 @@
 """Tests for verifying component setup"""
 import logging
 from collections import OrderedDict
+from typing import List
 
-import uuid
 import pytest
 
-from homeassistant.util import slugify
 from homeassistant.setup import async_setup_component
 from homeassistant.helpers import (
-    device_registry,
+    device_registry as dev_reg,
     entity_registry as ent_reg,
 )
 
 from pytest_homeassistant_custom_component.common import (
     MockConfigEntry,
-    mock_area_registry,
-    mock_device_registry,
-    mock_registry,
 )
 from custom_components.auto_areas.auto_area import AutoArea
-
 from custom_components.auto_areas.const import DOMAIN
+from tests.conftest import create_entity
 
 _LOGGER = logging.getLogger(__name__)
-AREAS = ("Kitchen", "Living Room", "Bathroom", "Bedroom")
 
 
-@pytest.fixture(name="areas", autouse=True)
-def fixture_areas(hass) -> dict:
-    """Areas"""
-    area_registry = mock_area_registry(hass)
-    areas = OrderedDict()
-    for area in AREAS:
-        created_area = area_registry.async_create(area)
-        areas[slugify(area)] = created_area
-
-    return areas
-
-
-@pytest.fixture(name="entity_registry")
-def fixture_entity_registry(hass, areas) -> ent_reg.EntityRegistry:
+@pytest.fixture(name="entities")
+def fixture_entity_registry(areas, entity_registry) -> List[ent_reg.RegistryEntry]:
     """Entities assigned to areas"""
-    entities = {
-        "light.entrance": ent_reg.RegistryEntry(
-            entity_id="light.entrance",
-            unique_id=str(uuid.uuid4()),
-            platform="test",
-        ),
-        "light.bathroom": ent_reg.RegistryEntry(
-            entity_id="light.bathroom",
-            unique_id=str(uuid.uuid4()),
-            platform="test",
-            area_id=areas["bathroom"].id,
-        ),
-        "light.bedroom": ent_reg.RegistryEntry(
-            entity_id="light.bedroom",
-            unique_id=str(uuid.uuid4()),
-            platform="test",
-            area_id=areas["bedroom"].id,
-        ),
-        "light.living_room": ent_reg.RegistryEntry(
-            entity_id="light.living_room",
-            unique_id=str(uuid.uuid4()),
-            platform="test",
-            area_id=areas["living_room"].id,
-        ),
-        "light.living_room_2": ent_reg.RegistryEntry(
-            entity_id="light.living_room_2",
-            unique_id=str(uuid.uuid4()),
-            platform="test",
-            area_id=areas["living_room"].id,
-        ),
-    }
-    return mock_registry(
-        hass,
-        entities,
-    )
+    return [
+        create_entity(entity_registry, "light", unique_id="1", area_id="living_room"),
+        create_entity(entity_registry, "light", unique_id="2", area_id="living_room"),
+        create_entity(entity_registry, "light", unique_id="1", area_id="bedroom"),
+        create_entity(entity_registry, "light", unique_id="1", area_id="bathroom"),
+    ]
 
 
 @pytest.fixture(name="devices")
-async def fixture_devices(hass, areas, entity_registry: ent_reg.EntityRegistry) -> None:
+async def fixture_devices(
+    areas,
+    device_registry: dev_reg.DeviceRegistry,
+    entity_registry: ent_reg.EntityRegistry,
+) -> None:
     """Entity which is assigned to an area via a device"""
-    registry = mock_device_registry(hass)
     bedroom_area_id = areas["bedroom"].id
     config_entry = MockConfigEntry(domain="light")
 
     # Create device
-    device = registry.async_get_or_create(
+    device = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
-        connections={(device_registry.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
+        connections={(dev_reg.CONNECTION_NETWORK_MAC, "12:34:56:AB:CD:EF")},
         identifiers={("bridgeid", "0123")},
-        entry_type=device_registry.DeviceEntryType.SERVICE,
+        entry_type=dev_reg.DeviceEntryType.SERVICE,
         sw_version="sw-version",
         name="name",
         manufacturer="manufacturer",
         model="model",
     )
-    registry.async_update_device(device.id, area_id=bedroom_area_id)
-
-    # Create entity for device
-    entity_registry.async_get_or_create(
-        "light",
-        "hue",
-        str(uuid.uuid4()),
-        device_id=device.id,
-    )
+    device_registry.async_update_device(device.id, area_id=bedroom_area_id)
+    create_entity(
+        entity_registry, "light", "bathroom2", area_id=None, device_id=device.id
+    ),
 
 
-async def test_area_initialization(hass, areas, entity_registry, devices):
+async def test_area_initialization(hass, entities, devices):
     """Verify area initialization"""
 
     # setup component
@@ -122,5 +74,5 @@ async def test_area_initialization(hass, areas, entity_registry, devices):
     # verify entities
     assert len(auto_areas["kitchen"].entities) is 0
     assert len(auto_areas["living_room"].entities) is 2
-    assert len(auto_areas["bathroom"].entities) is 1
     assert len(auto_areas["bedroom"].entities) is 2
+    assert len(auto_areas["bathroom"].entities) is 1
